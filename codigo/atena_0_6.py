85d03a42c580c28726783c12987181d47c42b7cb
Oziel Junior <58310583+oziieljuniior@users.noreply.github.com>
2022-10-28 23:21:00 -0400

    Create atena_0_6.py

---
+++ codigo/atena_0_6.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+from lxml import html
+import requests
+
+import numpy as np
+
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Dados"
+#print(arquivo_excel.sheetnames)
+ws['A1'] = "Local$Editora"
+ws['B1'] = "codigo"
+ws['C1'] = "Ano$Edicao"
+r = 2
+
+root = tk.Tk()
+root.withdraw()
+caminho = filedialog.askdirectory()
+
+#caminho.geometry("1020x720")
+
+
+lista_caminho = os.listdir(caminho)
+t = len(lista_caminho)
+
+a = []
+b = []
+d = []
+for i in range(0,t):
+    print("#############-----------------#############-----------------")
+    r = str(r)
+    print("Conversao do i: ", i + 1)
+    c = lista_caminho[i].split('_')
+    c[1] = c[1].replace('.pdf','')
+    a.append(c[0])
+    b.append(c[1])
+    print(type(b[i]),b[i])
+    
+    #open it, go to a website, and get results
+    name = 'https://digital.iai.spk-berlin.de/viewer/image/' + b[i] + '/1/LOG_0003/'
+    print(name)
+    url = requests.get(name)
+    code = url.status_code
+    print(code)
+    tree = html.fromstring(url.content)
+    titulo = tree.xpath('/html/body/div[3]/div/div/aside/div[4]/dl/dd/text()')
+    print(titulo)
+    titulo[0] = titulo[0].replace("\n\t\t\t\t\t\t\t\t\t\t", "")
+    titulo[0] = titulo[0].replace(".", "")
+    d.append(titulo[0])
+    print(titulo[0])
+    ##-----Planilha
+    o = 'A' + r
+    p = 'B' + r
+    q = 'C' + r
+    ws[o] = c[0]
+    ws[p] = c[1]
+    ws[q] = d[i]
+    wb.save("dados.xlsx")
+    r = int(r)
+    r += 1
+    print("Consulta realizada com sucesso, converteu i:", i + 1)
+    
+    print("Total de conversões:", t)
+    
+M0 = np.vstack((a,b,d))
+M1 = M0.T
+print(M1)
+print(lista_caminho)
+caminho_salvar = filedialog.askdirectory()
+
+for j in range(0,t):
+    ###------Renomear
+    #
+    #/home/oziel/Documentos/teste1/Mercurio peruano, Lima_828531196.pdf
+    antigo_nome = caminho + "/" + lista_caminho[j]
+    novo_nome = caminho_salvar + "/" + a[j] +"_" + titulo[0] + '.pdf'
+    #antigo_nome = lista_caminho[i]
+    #novo_nome = a[i] +"_" + titulo[0] + '.pdf'
+    
+    print(antigo_nome)
+    print(novo_nome)
+    os.rename(antigo_nome ,novo_nome)
+print("Conversão finalizada")
